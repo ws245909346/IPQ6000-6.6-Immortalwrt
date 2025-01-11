1382bc25df8f20f1e8924bc8030299809f471f8b
ws245909346 <102574232+ws245909346@users.noreply.github.com>
2025-01-11 11:52:32 +0800

    Update Patches.sh

--- Patches/Patches.sh
+++ Patches/Patches.sh
@@ -3,7 +3,6 @@
 if [[ $WRT_REPO == *"lede"* ]]; then
 	sed -i "/#src/d" ./wrt/feeds.conf.default
 	sed -i "s|\(src-git luci\).*|\1 https://github.com/coolsnowwolf/luci.git;openwrt-23.05|g" ./wrt/feeds.conf.default
-	sed -i '$a src-git lucky https://github.com/sirpdboy/luci-app-lucky' ./wrt/feeds.conf.default 
 
 	echo "$WRT_REPO patch has been installed!"
 fi
